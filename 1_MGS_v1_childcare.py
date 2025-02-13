232ad7b7f52e3bd3474714305fb082e2ccea747e
limc4 <limc4@middleton.school.nz>
2025-02-14 09:14:34 +1300

    drop off a child function

---
+++ 1_MGS_v1_childcare.py
@@ -0,0 +1,13 @@
+"""MGS Childcare
+Program for child day-care centre - version 1 - drop off a child function
+created by Charlotte"""
+
+def drop_off():
+    name_ = input("Please enter the name of the child: ")
+    roll.append(name_)
+    print(f"\n{name_} has been added to the roll.\n")
+
+# Main routine
+roll = []
+
+drop_off()
