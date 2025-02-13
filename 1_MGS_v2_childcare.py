5a35e5d16046deec86b6b7682131895749963758
limc4 <limc4@middleton.school.nz>
2025-02-14 09:26:43 +1300

    Main welcome screen added

---
+++ 1_MGS_v2_childcare.py
@@ -0,0 +1,33 @@
+"""MGS Childcare
+Program for child day-care centre - version 1 - main function
+created by Charlotte"""
+
+def drop_off():
+    name_ = input("Please enter the name of the child: ")
+    roll.append(name_)
+    print(f"\n{name_} has been added to the roll.\n")
+
+# Main routine
+roll = []
+choice = 0
+
+while choice != 5:
+    print("=" * 60)
+    print("What would you like to do?")
+    print("=" * 60)
+    print()
+    print("1 Drop off a child")
+    print()
+    choice = int(input("Enter your choice (number from 1 to 5): "))
+    print()
+
+    if choice == 1:
+        drop_off()
+    elif choice == 2:
+        pass
+    elif choice == 3:
+        pass
+    elif choice == 4:
+        pass
+    else:
+        print("Goodbye")
